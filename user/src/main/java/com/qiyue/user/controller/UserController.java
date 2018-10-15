ecbd3ef2e8d5839f35f79759ee1bab7bf2df6e83
muxianliangqin <muxianliangqin@users.noreply.github.com>
2018-10-15 20:06:50 +0800

    添加测试

--- user/src/main/java/com/qiyue/user/controller/UserController.java
+++ user/src/main/java/com/qiyue/user/controller/UserController.java
@@ -1,10 +1,8 @@
 package com.qiyue.user.controller;
 
-import com.qiyue.user.node.Node;
 import com.qiyue.user.service.UserService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.web.bind.annotation.*;
 
@@ -12,15 +10,9 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @EnableDiscoveryClient
 public class UserController {
-    @Autowired
-    private DiscoveryClient discoveryClient;
+
     @Autowired
     private UserService userService;
-
-    @RequestMapping(value = "/test", method = RequestMethod.GET)
-    public String test(){
-        return "services:" + discoveryClient.getInstances("qiyue-cloud-crawler");
-    }
 
     @GetMapping("/getMenuNode")
     public String getMenuNode(int id){
--- user/src/test/java/com/qiyue/user/UserApplicationTests.java
+++ user/src/test/java/com/qiyue/user/UserApplicationTests.java
@@ -2,21 +2,53 @@ package com.qiyue.user;
 
 import com.qiyue.user.controller.UserController;
 import com.qiyue.user.node.Node;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.MockitoAnnotations;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class UserApplicationTests {
     @Autowired
+    protected WebApplicationContext webApplicationContext;
+    @Autowired
     private UserController userController;
 
+    private MockMvc mockMvc;
+
+    @Before
+    public void setUp() {
+        MockitoAnnotations.initMocks(this);
+        mockMvc = MockMvcBuilders.standaloneSetup(webApplicationContext).build();
+    }
+
     @Test
-    public void contextLoads() {
+    public void contextLoads() throws Exception {
         String menuTree = userController.getMenuNode(1);
         System.out.println(menuTree);
+
+        MvcResult result = mockMvc
+                .perform(MockMvcRequestBuilders.post("/getMenuNode")
+                        .accept(MediaType.APPLICATION_JSON)
+                        .param("id", "1"))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andReturn();
     }
 }
