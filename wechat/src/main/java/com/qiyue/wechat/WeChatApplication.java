9d72411a26d15fa9473fb48d5350b28cf1f7f1cd
muxianliangqin <34302273+muxianliangqin@users.noreply.github.com>
2019-05-15 17:03:39 +0800

    --

--- wechat/src/main/java/com/qiyue/wechat/WeChatApplication.java
+++ wechat/src/main/java/com/qiyue/wechat/WeChatApplication.java
@@ -9,7 +9,7 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 public class WeChatApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(WechatApplication.class, args);
+        SpringApplication.run(WeChatApplication.class, args);
     }
 
 }
