f2ce464e0a32d0a12dded0022912edf406c3b4da
muxianliangqin <muxianliangqin@users.noreply.github.com>
2020-03-18 18:07:09 +0800

    --

--- gateway/src/main/java/com/qiyue/GatewayApplication.java
+++ gateway/src/main/java/com/qiyue/GatewayApplication.java
@@ -3,10 +3,12 @@ package com.qiyue;
 import com.qiyue.common.constant.Constant;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 import org.springframework.session.data.redis.RedisFlushMode;
 import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 
+@RefreshScope
 @SpringBootApplication
 @EnableZuulProxy
 @EnableRedisHttpSession(redisNamespace = Constant.SESSION_REDIS_NAME_SPACE, redisFlushMode = RedisFlushMode.IMMEDIATE)
