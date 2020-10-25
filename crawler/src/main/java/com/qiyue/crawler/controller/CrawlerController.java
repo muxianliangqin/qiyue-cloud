f93deb3151a98040bd7de504254274c03312f0ec
muxianliangqin <muxianliangqin@users.noreply.github.com>
2020-10-25 22:00:04 +0800

    beta-0.0.1

--- crawler/src/main/java/com/qiyue/crawler/controller/CrawlerController.java
+++ crawler/src/main/java/com/qiyue/crawler/controller/CrawlerController.java
@@ -5,6 +5,9 @@ import com.qiyue.crawler.model.param.PluginCrawlerParam;
 import com.qiyue.crawler.service.CrawlerService;
 import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 @RestController
 public class CrawlerController {
     private final CrawlerService crawlerService;
@@ -21,7 +24,11 @@ public class CrawlerController {
      */
     @CrossOrigin(allowCredentials = "true", methods = {RequestMethod.GET, RequestMethod.POST, RequestMethod.PUT})
     @RequestMapping("/plugin/save")
-    public Response<String> savePluginCrawler(@RequestBody PluginCrawlerParam crawlerParam) {
+    public Response<String> savePluginCrawler(@RequestBody PluginCrawlerParam crawlerParam,
+                                              HttpServletRequest request, HttpServletResponse response) {
+        response.setHeader("Access-Control-Allow-Origin", request.getHeader("Origin"));
+        response.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+        response.setHeader("Access-Control-Allow-Credentials", "true");
         return crawlerService.savePluginCrawler(crawlerParam);
     }
 
