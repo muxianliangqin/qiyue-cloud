aea8bfa0062bff76cf94290602ecec70e173005a
muxianliangqin <muxianliangqin@users.noreply.github.com>
2020-10-26 22:48:18 +0800

    beta-0.0.1

--- crawler/src/main/java/com/qiyue/crawler/controller/CrawlerController.java
+++ crawler/src/main/java/com/qiyue/crawler/controller/CrawlerController.java
@@ -5,9 +5,6 @@ import com.qiyue.crawler.model.param.PluginCrawlerParam;
 import com.qiyue.crawler.service.CrawlerService;
 import org.springframework.web.bind.annotation.*;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
 @RestController
 public class CrawlerController {
     private final CrawlerService crawlerService;
@@ -22,7 +19,6 @@ public class CrawlerController {
      * @param crawlerParam 爬取结果
      * @return 接口执行结果
      */
-    @CrossOrigin(allowCredentials = "true", methods = {RequestMethod.GET, RequestMethod.POST, RequestMethod.PUT})
     @RequestMapping("/plugin/save")
     public Response<String> savePluginCrawler(@RequestBody PluginCrawlerParam crawlerParam) {
         return crawlerService.savePluginCrawler(crawlerParam);
